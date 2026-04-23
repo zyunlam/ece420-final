e0c3abb5c09b6745dc860312911cacede7335fd8
EhWhoAmI <eh@ehwhoami.com>
2026-04-23 13:46:24 -0500

    Add fisherfaces

---
+++ app/src/main/cpp/finalproject.cpp
@@ -0,0 +1,17 @@
+// Write C++ code here.
+//
+// Do not forget to dynamically load the C++ library into your application.
+//
+// For instance,
+//
+// In MainActivity.java:
+//    static {
+//       System.loadLibrary("finalproject");
+//    }
+//
+// Or, in MainActivity.kt:
+//    companion object {
+//      init {
+//         System.loadLibrary("finalproject")
+//      }
+//    }
